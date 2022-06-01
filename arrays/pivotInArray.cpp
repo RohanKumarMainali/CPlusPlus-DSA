b57def047989399f8f3d731d54bd01ea9f7abcde
RohanKumarMainali <92391187+RohanKumarMainali@users.noreply.github.com>
2022-06-01 20:29:45 +0545

    Important Questions on array

---
+++ arrays/pivotInArray.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int getPivot(int arr[],int n){
+
+    int start=0;
+    int end=n-1;
+    int mid=start+(end-start)/2;
+
+    while(start<=end){
+        if(arr[mid]>=arr[start]){
+            start=mid+1;
+        }
+        else{
+            end=mid;
+        }
+        mid=start+(end-start)/2;
+    }
+    return start;
+
+}
+
+    int main(){
+        int arr[7]={5,7,8,10,12,1,2};
+        cout<<getPivot(arr,7);
+
+}
