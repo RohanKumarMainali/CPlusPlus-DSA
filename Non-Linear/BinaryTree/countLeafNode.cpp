5facd7d052a39036c719c6c91da6e7356e0563e5
984345 <rohanmainali39@gmail.com>
2023-01-11 10:50:53 +0545

    count leaf node problem

--- Non-Linear/BinaryTree/countLeafNode.cpp
+++ Non-Linear/BinaryTree/countLeafNode.cpp
@@ -1,1 +1,17 @@
-test
+// this problem is solved easily with the help of recursion in (inorder traversal)
+// time complexity = O(n) and space complexity = O(n)
+
+***********************************************************/
+void inOrder(BinaryTreeNode<int> * root, int & count){
+    if(root == NULL) return;
+    inOrder(root->left , count);
+    if(root->left == NULL & root->right==NULL) count ++;
+    inOrder(root->right , count);
+}
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+    int count = 0;
+    inOrder(root , count );
+    return count;
+}
