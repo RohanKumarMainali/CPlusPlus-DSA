90c0cdc2254c588b94d504a534d80c38f13e7076
984345 <rohanmainali39@gmail.com>
2023-01-15 09:07:21 +0545

    optimized solution

--- Non-Linear/BinaryTree/checkBinarayTreeBalanced.cpp
+++ Non-Linear/BinaryTree/checkBinarayTreeBalanced.cpp
@@ -9,7 +9,7 @@
 //5
 
 
-
+// time complexity - O(N^2)
 class Solution{
     public:
     //Function to check whether a binary tree is balanced or not.
@@ -35,5 +35,50 @@ class Solution{
         
         if(left && right && diff) return true;
         else return false;
+    }
+};
+
+ 
+ // time complexity = O(N)
+
+class Solution{
+    public:
+    //Function to check whether a binary tree is balanced or not.
+    
+    // rather than calculating height in another function call,
+    // here we calculate height and isBalanced at once for each node
+    
+    
+    pair<bool,int> isBalancedFast(Node * node){
+        
+        if(node == NULL) {
+            pair<bool,int> p = make_pair(true,0);
+            return p;
+        }
+        
+        // first value of pair -> isBalanced
+        // second value of pair -> height
+        
+        pair<bool , int> left = isBalancedFast(node->left);
+        pair<bool, int> right = isBalancedFast(node->right);
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        bool diff = abs(left.second - right.second) <= 1;
+
+        pair<bool,int> ans;
+        ans.second = max(left.second, right.second) + 1; 
+        
+        if(leftAns && rightAns && diff){
+            ans.first = true;
+        }
+        else ans.first = false;
+        
+        return ans;
+    }
+    
+    bool isBalanced(Node *root)
+    {
+    
+        return isBalancedFast(root).first;
     }
 };
